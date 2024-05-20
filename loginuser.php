c4fc3b74b198b4e9598838fcb3552e064458fc71
Latescholnihbozz <164280841+Latescholnihbozz@users.noreply.github.com>
2024-05-20 10:36:34 +0700

    Add files via upload
    menambahkan loginuserphp dan css biru

--- loginuser.php
+++ loginuser.php
@@ -1,8 +1,31 @@
 <?php
-require 'koneksi.php';
-$user = query("SELECT * FROM user");
+ session_start();
+ include 'koneksi.php';
+
+if(isset($_POST['login'])){
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $result = mysqli_query($kon, "SELECT * FROM loginpks 
+             WHERE username = '$username' and password = '$password'");
+             
+    $row = mysqli_fetch_array($result);
+
+if(is_array($row)){
+    $_SESSION['username'] = $row['username'];
+    $_SESSION['password'] = $row['password'];
+    header('Location:../user/userpks.php');
+}else{
+    echo "Login GAGAL";
+    }
+}
+
+
+
 
 ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -12,27 +35,29 @@ $user = query("SELECT * FROM user");
     <title>PKS login WEB</title>
     <link rel="stylesheet" href="loginuser.css" />
     <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
+
+
 </head>
 
 <body>
     <div class="wrapper">
         <header class="header">
-            <img src="smk8-removebg-preview.png" />
-            <h1 class="judul">PKS SMK N 8 SEMARANG</h1>
+            <img src="../user/smk8-removebg-preview.png" class="logo" />
+            <h1 class=>PKS SMK N 8 SEMARANG</h1>
         </header>
 
-        <form action="">
+        <form action="../user/userpks.php" method="post">
             <div class="input-box">
-                <input type="text" placeholder="Username" required />
+                <input type="text" placeholder="Username" required name="username" />
                 <i class="bx bxs-user"></i>
             </div>
 
             <div class="input-box">
-                <input type="password" placeholder="Password" required />
+                <input type="password" placeholder="Password" required name="password" />
                 <i class="bx bxs-lock-alt"></i>
             </div>
-            <!-- <a type="submit" class="btn">Login</a> -->
-            <a href="userpks.php" type="submit" class="btn" role="button">Login</a>
+            <button class="btn" type="submit" name="login">Login</button>
+
         </form>
     </div>
 </body>
